46820d5e5e83a38afd9efaec43dbcaa1fd74443e
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2022-08-18 18:42:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+
+function superbowlWin(record){
+    const win = (record) => record.result === "W";
+    const winning = record.find(win)
+    if (winning){
+        return winning.year
+    }else{
+        return undefined;
+    }
+}
+
+
+record.find(superbowlWin);
